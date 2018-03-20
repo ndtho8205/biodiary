2ea198e8c569bc804cd07755a1aea31c347a1aae
Tho Nguyen Duc <ndtho8205@gmail.com>
2018-03-20 16:40:49 +0700

    EntryEditorActivity styles.

--- app/src/main/java/edu/bk/thesis/biodiary/activities/EntryEditorActivity.java
+++ app/src/main/java/edu/bk/thesis/biodiary/activities/EntryEditorActivity.java
@@ -3,8 +3,10 @@ package edu.bk.thesis.biodiary.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
@@ -14,16 +16,25 @@ import edu.bk.thesis.biodiary.models.Diary;
 
 public class EntryEditorActivity extends AppCompatActivity
 {
-    private EditText    mEntryDate;
-    private EditText    mEntryContent;
-    private Diary.Entry mEntry;
-    private boolean     isNewEntry;
+    private Toolbar        mToolbar;
+    private ActionMenuView mMenuView;
+    private EditText       mEntryDate;
+    private EditText       mEntryContent;
+    private Diary.Entry    mEntry;
+    private boolean        isNewEntry;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_entry_editor, mMenuView.getMenu());
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
         switch (item.getItemId()) {
-            case android.R.id.home:
+            case R.id.action_editor_close:
                 if (isNewEntry) {
                     returnNewEntry();
                 }
@@ -54,6 +65,19 @@ public class EntryEditorActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_editor);
 
+        mToolbar = findViewById(R.id.editor_toolbar);
+        mMenuView = findViewById(R.id.editor_toolbar_amv);
+        mMenuView.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                return onOptionsItemSelected(item);
+            }
+        });
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitle(null);
+
         mEntryDate = findViewById(R.id.et_entry_date);
         mEntryContent = findViewById(R.id.et_entry_content);
 
@@ -69,9 +93,9 @@ public class EntryEditorActivity extends AppCompatActivity
             }
         }
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        // ActionBar actionBar = getSupportActionBar();
+        // if (actionBar != null) {
+        //     actionBar.setDisplayHomeAsUpEnabled(true);
+        // }
     }
 }
